0350e46a7ac9786d18e5904368074e99889ac3c6
dotnet9 <qq1012434131@gmail.com>
2021-10-25 00:16:03 +0800

    add autofac;

--- src/Lequ.Blog/Lequ.Blog/Program.cs
+++ src/Lequ.Blog/Lequ.Blog/Program.cs
@@ -1,33 +1,16 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Lequ.Blog.Extensions.ServiceExtensions;
-using Lequ.Blog.IRepository;
-using Lequ.Blog.IService;
-using Lequ.Blog.Repository;
-using Lequ.Blog.Service;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacModuleRegister()));
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddScoped<IAboutRepository, AboutRepository>();
-builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICommentRepository, CommentRepository>();
-builder.Services.AddScoped<IContactRepository, ContactRepository>();
-builder.Services.AddScoped<IUserInfoRepository, UserInfoRepository>();
-
-builder.Services.AddScoped<IAboutService, AboutService>();
-builder.Services.AddScoped<IBlogService, BlogService>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<ICommentService, CommentService>();
-builder.Services.AddScoped<IContactService, ContactService>();
-builder.Services.AddScoped<IUserInfoService, UserInfoService>();
-
-builder.Services.AddDbContext<Context>(option => {
-    option.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=Lequ.Blog;integrated security=true;");
-});
+builder.Services.AddDbSetup();
 builder.Services.AddAutoMapperSetup();
 
 var app = builder.Build();
