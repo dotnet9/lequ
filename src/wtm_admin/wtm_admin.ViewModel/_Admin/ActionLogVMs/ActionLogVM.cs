7799006bf01151bfc5fb0db76036340856e00db0
liushiquan <liushiquan@islide.cc>
2021-10-26 10:09:35 +0800

    remove unuse project;

--- src/wtm_admin/wtm_admin.ViewModel/_Admin/ActionLogVMs/ActionLogVM.cs
+++
@@ -1,9 +0,0 @@
-// WTM默认页面 Wtm buidin page
-using WalkingTec.Mvvm.Core;
-
-namespace WalkingTec.Mvvm.Mvc.Admin.ViewModels.ActionLogVMs
-{
-    public class ActionLogVM : BaseCRUDVM<ActionLog>
-    {
-    }
-}
