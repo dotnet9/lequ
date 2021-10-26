514a8d8046dc2c6c332e244af5fb5e29f1020eed
dotnet9 <qq1012434131@gmail.com>
2021-10-27 02:07:32 +0800

    add admin template;

---
+++ src/Lequ.Blog/Lequ.Blog/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lequ.Blog.Controllers
+{
+    public class AdminController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return await Task.FromResult(View());
+        }
+
+        public async Task<IActionResult> UserProfile()
+        {
+            return await Task.FromResult(View());
+        }
+
+        public async Task<IActionResult> UserEmail()
+        {
+            return await Task.FromResult(View());
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Test()
+        {
+            return await Task.FromResult(View());
+        }
+
+        [AllowAnonymous]
+        public async Task<PartialViewResult> NavbarPartial()
+        {
+            return await Task.FromResult(PartialView());
+        }
+
+        [AllowAnonymous]
+        public async Task<PartialViewResult> SidebarPartial()
+        {
+            return await Task.FromResult(PartialView());
+        }
+
+        [AllowAnonymous]
+        public async Task<PartialViewResult> FooterPartial()
+        {
+            return await Task.FromResult(PartialView());
+        }
+    }
+}
---
+++ src/Lequ.Blog/Lequ.Blog/ViewComponents/Admin/MessageNotification.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Lequ.Blog.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lequ.Blog.ViewComponents.Admin
+{
+    public class MessageNotification : ViewComponent
+    {
+        private readonly IUserInfoService _service;
+        private readonly IMapper _mapper;
+
+        public MessageNotification(IUserInfoService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        public IViewComponentResult Invoke(int id)
+        {
+            //var posts = _service.ToListByUserIDAsync(1).Result;
+            //var postDtos = _mapper.Map<IEnumerable<BlogDto>>(posts);
+            return View();
+        }
+    }
+}
