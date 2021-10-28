57d999e25c83f7160776c478937c3e8ca8f90220
dotnet9 <qq1012434131@gmail.com>
2021-10-29 01:07:42 +0800

    add two module to admin dashboard;

---
+++ src/Lequ.Blog/Lequ.Blog/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Lequ.Blog.IService;
+using Lequ.Blog.Model.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lequ.Blog.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        private readonly IBlogService _service;
+        private readonly IMapper _mapper;
+
+        public BlogListDashboard(IBlogService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        public IViewComponentResult Invoke(int id)
+        {
+            var posts = _service.ToListWithCategoryAsync().Result;
+            var postDtos = _mapper.Map<IEnumerable<BlogDto>>(posts);
+            return View(postDtos);
+        }
+    }
+}
---
+++ src/Lequ.Blog/Lequ.Blog/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Lequ.Blog.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lequ.Blog.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        private readonly ICategoryService _service;
+        private readonly IMapper _mapper;
+
+        public CategoryListDashboard(ICategoryService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = _service.GetAllAsync().Result;
+            return View(values);
+        }
+    }
+}
