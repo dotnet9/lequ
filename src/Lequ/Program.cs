e87eea53efc1e962a601a965b0f93e1d36f32483
dotnet9 <1012434131@qq.com>
2021-11-04 16:45:48 +0800

    add loading style;

--- src/Lequ/Program.cs
+++ src/Lequ/Program.cs
@@ -44,6 +44,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Blog}/{action=Index}/{id?}");
 
 app.Run();
