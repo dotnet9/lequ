8f96265606049cc3fa16f6828fc39774788add5e
dotnet9 <1012434131@qq.com>
2021-11-15 16:13:42 +0800

    update footer component use;

--- src/dotnet_blog/Lequ/ServiceExtensions/DbSetup.cs
+++ src/dotnet_blog/Lequ/ServiceExtensions/DbSetup.cs
@@ -1,16 +1,16 @@
 ﻿using Lequ.Repository;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Lequ.ServiceExtensions
 {
-    public static class DbSetup
+	public static class DbSetup
     {
         public static void AddDbSetup(this IServiceCollection services, string connectionStr)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
 
-            services.AddDbContext<LequDbContext>(option => { option.UseSqlServer(connectionStr); });
+            //services.AddDbContext<LequDbContext>(option => { option.UseSqlServer(connectionStr); });
+            services.AddDbContext<LequDbContext>(option => { option.UseSqlite(connectionStr); });
         }
     }
 }
