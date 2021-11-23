55e6d4981a5f4cdfaebea13dd91b6336a0cfc13c
dotnet9 <1012434131@qq.com>
2021-11-23 22:33:55 +0800

    Add category and tag components;
    1. Adjust the layout of the article details page;
    2. Add all category components;
    3. Add all tag components;

---
+++ src/dotnet_blog/Lequ/ViewComponents/Tag/TagList.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Lequ.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lequ.ViewComponents.Tag;
+
+public class TagList : ViewComponent
+{
+    private readonly IMapper _mapper;
+    private readonly ITagService _service;
+
+    public TagList(ITagService service, IMapper mapper)
+    {
+        _service = service;
+        _mapper = mapper;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var value = await _service.SelectAsync(x => x.BlogTags);
+        return View((from p in value orderby p.BlogTags?.Count descending select p).ToList());
+    }
+}
