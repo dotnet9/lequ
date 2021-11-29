c675989c61fba79d8c1b9f0af3122cd7bf1b5c0c
Lequ.CO <1012434131@qq.com>
2021-11-29 15:09:59 +0800

    Add tag manager(Unfinished to be continued);

--- src/dotnet_blog/Lequ/Controllers/TagController.cs
+++ src/dotnet_blog/Lequ/Controllers/TagController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lequ.IService;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Lequ.Controllers;
 
 public class TagController : Controller
 {
+	private readonly ITagService _tagService;
+
+	public TagController(ITagService tagService)
+	{
+		_tagService = tagService;
+	}
+
 	public IActionResult Index()
 	{
 		return View();
